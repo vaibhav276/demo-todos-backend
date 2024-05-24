d87cdde1f04039aab8a155807f0ac81cd6970890
Vaibhav Pujari <276vaibhav+github@gmail.com>
2024-05-24 17:49:19 +0530

    Added a condition in contextLoads test

--- src/test/java/com/demo/demotodos/DemoTodosApplicationTests.java
+++ src/test/java/com/demo/demotodos/DemoTodosApplicationTests.java
@@ -1,13 +1,21 @@
 package com.demo.demotodos;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.demo.demotodos.controller.TodosV1Controller;
 
 @SpringBootTest
 class DemoTodosApplicationTests {
 
+	@Autowired
+	TodosV1Controller todosController;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(todosController).isNotNull();
 	}
 
 }
